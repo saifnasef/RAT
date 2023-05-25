648fd85a273e7ea38bccab887810a659e3158223
saif <saif.aboueldahab@gmail.com>
2023-05-25 23:45:17 +0300

    sa''

--- secondary.ps1
+++
@@ -1,4 +0,0 @@
-
-mkdir C:/temp
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/temp/python-3.7.0.exe"
-c:/temp/python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
